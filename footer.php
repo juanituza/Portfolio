d66206619317348efab597ad31324e33c2f60be7
juanituza <juanituza85@gmail.com>
2022-04-24 14:31:27 -0300

    cambios 24-4

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
 <footer class="container">
     <div class="row mt-4 pb-1">
         <div class="col-12 col-sm-3 text-sm-start wrapper">
-            <div class="icon">
-                <a href="https://github.com/" target="_blank"><i class="fa-brands fa-github  "></i></a>
-            </div>
-            <div class="icon">
-                <a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin  "></i></a>
-            </div>
+            <a href="https://github.com/" target="_blank">
+                <div class="icon">
+                    <i class="fa-brands fa-github  "></i>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank">
+                <div class="icon">
+                    <i class="fa-brands fa-linkedin  "></i>
+                </div>
+            </a>
         </div>
         <div class="col-12 col-sm-3  pt-3 m-sm-3 p-sm-0">
             Sponsor: <a href="https://depcsuite.com" target="_blank">DePC</a>
